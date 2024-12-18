dd69457a1f956dac5e1fe89e391da493aacc08ce
marc2k3 <marc2003@live.co.uk>
2024-12-18 06:27:04 +0000

    Update ScintillaImpl.mjs

--- jsp3/ScintillaImpl.mjs
+++ jsp3/ScintillaImpl.mjs
@@ -39,9 +39,15 @@ const basicTypes = [
 ]
 
 function format(name, type, ptr) {
-	if (!name.length) return '0'
-	if (type.endsWith('*')) return `reinterpret_cast<${ptr}>(${name})`
-	if (!basicTypes.includes(type)) return `static_cast<${ptr}>(${name})`
+	if (!name.length)
+		return '0'
+
+	if (type.endsWith('*'))
+		return `reinterpret_cast<${ptr}>(${name})`
+
+	if (!basicTypes.includes(type))
+		return `static_cast<${ptr}>(${name})`
+
 	return name
 }
 
@@ -52,8 +58,34 @@ function get_args(line) {
 }
 
 function get_type(type) {
-	if (!type) return ''
+	if (!type)
+		return ''
+
 	return typeAliases[type] || type
+}
+
+function build_call(name, typeWp, nameWp, typeLp, nameLp, ret) {
+	let call;
+
+	if (typeLp == 'const char*')
+		call = `CallString(Message::${name}, ${format(nameWp, typeWp, 'uintptr_t')}, ${nameLp})`
+	else if (typeLp.endsWith('*'))
+		call = `CallPointer(Message::${name}, ${format(nameWp, typeWp, 'uintptr_t')}, ${nameLp})`
+	else if (typeLp.length)
+		call = `Call(Message::${name}, ${format(nameWp, typeWp, 'uintptr_t')}, ${format(nameLp, typeLp, 'intptr_t')})`
+	else if (typeWp.length)
+		call = `Call(Message::${name}, ${format(nameWp, typeWp, 'uintptr_t')})`
+	else
+		call = `Call(Message::${name})`
+
+	if (ret == 'void*')
+		return `return reinterpret_cast<void*>(${call});`
+	else if (!basicTypes.includes(ret) || ret == 'int' || ret == 'Colour' || ret == 'ColourAlpha')
+		return `return static_cast<${ret}>(${call});`
+	else if (ret != 'void')
+		return `return ${call};`
+	else
+		return `${call};`
 }
 
 function create_body(content) {
@@ -63,7 +95,8 @@ function create_body(content) {
 	let tmp = []
 
 	for (const line of lines) {
-		if (line.startsWith('cat Deprecated')) break
+		if (line.startsWith('cat Deprecated'))
+			break
 
 		const arr = line.substr(0, line.indexOf('=')).split(' ')
 		const feature = arr[0]
@@ -77,35 +110,23 @@ function create_body(content) {
 			const nameWp = wp[1] || ''
 			const typeLp = get_type(lp[0])
 			const nameLp = lp[1] || ''
-
 			let str = `\t${ret} ${name}(`
-			if (typeWp.length) str += `${typeWp} ${nameWp}`
-			if (typeWp.length && typeLp.length) str += ', '
-			if (typeLp.length) str += `${typeLp} ${nameLp}`
-			str += ') { '
 
-			let call;
-			if (typeLp == 'const char*') {
-				call = `CallString(Message::${name}, ${format(nameWp, typeWp, 'uintptr_t')}, ${nameLp})`
-			} else if (typeLp.endsWith('*')) {
-				call = `CallPointer(Message::${name}, ${format(nameWp, typeWp, 'uintptr_t')}, ${nameLp})`
-			} else if (typeLp.length) {
-				call = `Call(Message::${name}, ${format(nameWp, typeWp, 'uintptr_t')}, ${format(nameLp, typeLp, 'intptr_t')})`
-			} else if (typeWp.length) {
-				call = `Call(Message::${name}, ${format(nameWp, typeWp, 'uintptr_t')})`
-			} else {
-				call = `Call(Message::${name})`
-			}
+			if (typeWp.length)
+				str += `${typeWp} ${nameWp}`
 
-			if (ret == 'void*') str += `return reinterpret_cast<void*>(${call})`
-			else if (!basicTypes.includes(ret) || ret == 'int' || ret == 'Colour' || ret == 'ColourAlpha') str += `return static_cast<${ret}>(${call})`
-			else if (ret != 'void') str += `return ${call}`
-			else str += call
+			if (typeWp.length && typeLp.length)
+				str += ', '
 
-			str += '; }'
+			if (typeLp.length)
+				str += `${typeLp} ${nameLp}`
+
+			const call = build_call(name, typeWp, nameWp, typeLp, nameLp, ret)
+			str += `) { ${call} }`
 			tmp.push(str)
 		}
 	}
+
 	tmp.sort()
 	return tmp.join(CRLF)
 }
